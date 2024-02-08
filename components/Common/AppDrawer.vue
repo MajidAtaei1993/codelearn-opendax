96b8f4a71906614c0cda8cac7d742ba755901b1a
MajidAtaei1993 <majid.ataie79@gmail.com>
2024-02-08 11:15:27 +0330

    apexcharts

--- components/Common/AppDrawer.vue
+++ components/Common/AppDrawer.vue
@@ -22,6 +22,43 @@
                 </v-list-item>
             </v-list-group>
         </v-list>
+        <template v-slot:append>
+            <v-list nav>
+                <v-list-item title="0.000244 ETH" subtitle="0xGD63...6DS9">
+                    <template v-slot:prepend>
+                        <v-avatar size="30">
+                            <v-img src="@/assets/images/coin-mark.png" />
+                        </v-avatar>
+                    </template>
+                </v-list-item>
+                <v-list-item>
+                    <template v-slot:prepend>
+                        <v-list-item-title class="d-flex flex-wrap" v-if="!events.expandOnHover">
+                            <v-btn width="48%" variant="outlined">deposit</v-btn>
+                            <v-spacer />
+                            <v-btn width="48%" color="secondary">withdraw</v-btn>
+                        </v-list-item-title>
+                        <v-list-item-title class="flex-column" v-else>
+                            <v-btn size="small" variant="text" icon>
+                                <img src="@/assets/icons/withdraw-icon.png" alt="">
+                            </v-btn>
+                            <v-spacer />
+                            <v-btn size="small" variant="text" icon>
+                                <img src="@/assets/icons/deposit-icon.png" alt="">
+                            </v-btn>
+                        </v-list-item-title>
+                    </template>
+                </v-list-item>
+                <v-list-item title="collapse">
+                    <template v-slot:prepend>
+                        <v-btn size="small" :icon="events.expandOnHover ? 'mdi-arrow-collapse-left' : 'mdi-arrow-collapse-right'" variant="text" @click="events.expandOnHover =! events.expandOnHover"></v-btn>
+                    </template>
+                    <template v-slot:append>
+                        <v-btn size="small" icon="mdi-logout-variant" variant="text"></v-btn>
+                    </template>
+                </v-list-item>
+            </v-list>
+        </template>
     </v-navigation-drawer>
 </template>
 
@@ -48,6 +85,7 @@
                     { icon:'', text:'withdraw', link:'' },
                 ]
             },
+            { icon:'mdi-cog', text:'settings', link:'' },
         ],
     })
 </script>
